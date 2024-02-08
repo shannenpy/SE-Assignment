8dd21eb07c0460d94b47676279e5fbeb76c4f764
limlitongchloe <s10242590@connect.np.edu.sg>
2024-02-08 10:13:24 +0800

    pushing my changes to classes and program.cs

--- SE Assignment/SE Assignment/SeasonParkingPass.cs
+++ SE Assignment/SE Assignment/SeasonParkingPass.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 
 namespace SE_Assignment
 {
-    interface SeasonParkingPass
+    abstract class SeasonParkingPass
     {
-        Vehicle Vehicle { get; set; }
-        int PassID { get; set; }
-        void setVehicle(Vehicle v);
-        void setParkingStatus(string s);
-        string getParkingStatus();
-        void setValidityStatus(string s);
-        string getValidityStatus();
-        string ParkStatus { get; set; }
-        string ValidityStatus { get; set; }
-        int generatePassID();
+        public abstract Vehicle Vehicle { get; set; }
+        public abstract int PassID { get; set; }
+        public abstract void setVehicle(Vehicle v);
+        public abstract void setParkingStatus(string s);
+        public abstract string getParkingStatus();
+        public abstract void setValidityStatus(string s);
+        public abstract string getValidityStatus();
+        public abstract string ParkStatus { get; set; }
+        public abstract string ValidityStatus { get; set; }
+        public abstract int LatestPassID { get; set; }
+        public abstract int generatePassID();
+        public abstract string passType();
+
     }
 }
