cd0fc4359d742054a9003bfbbc423189d4245cbb
05 - Elizabeth Tan Yip Zhen <s10242919@connect.np.edu.sg>
2024-02-09 00:32:52 +0800

    Apply Season use case completed

--- SE Assignment/SE Assignment/Application.cs
+++ SE Assignment/SE Assignment/Application.cs
@@ -8,24 +8,39 @@ namespace SE_Assignment
 {
     class Application
     {
-        private User user { get; set; }
+        private Guid id {  get; set; }
         private Guid applicationId { get; set; }
-        private DateTime startMonth { get; set; } // store month only? or whole date?
+        private int type { get; set; } // 1 for daily, 2 for monthly
+        private DateTime startMonth { get; set; }
         private DateTime endMonth { get; set; }
         private bool paid { get; set; } // 0 for false, 1 for true
+        private string paymentMode { get; set; }
+        private User user { get; set; }
+        private Vehicle vehicle { get; set; }
 
-        public Application(DateTime startMonth, DateTime endMonth, bool paid = false, User user = null)
+        public Application(int type, DateTime startMonth, DateTime endMonth, string paymentMode, User user = null, Vehicle vehicle = null)
         {
-            this.user = user;
+            this.id = Guid.NewGuid();
             this.applicationId = Guid.NewGuid();
+            this.type = type;
             this.startMonth = startMonth;
             this.endMonth = endMonth;
-            this.paid = paid;
+            this.paymentMode = paymentMode;
+            this.paid = false;
+            this.user = user;
+            this.vehicle = vehicle;
         }
 
-        public void setPaid(bool paid)
+        public bool makePayment()
         {
-            this.paid = paid;
+            // implementation
+            Console.Write("Hit any key to execute Make Payment use case.");
+            Console.ReadKey();
+            Console.WriteLine();
+            // if payment succeeds
+            paid = true;
+            Console.WriteLine("Make Payment use case executed successfully.");
+            return paid;
         }
     }
 }
